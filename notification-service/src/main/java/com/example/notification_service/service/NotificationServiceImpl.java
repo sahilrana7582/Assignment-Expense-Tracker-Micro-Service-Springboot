d16279b7b1f36865c0372b969813fa2f113e5c3c
Sahil Rana <sahilrana27582@gmail.com>
2025-04-29 22:09:01 +0530

    Notification Service | Kafka Communication Implemented

--- notification-service/src/main/java/com/example/notification_service/service/NotificationServiceImpl.java
+++ notification-service/src/main/java/com/example/notification_service/service/NotificationServiceImpl.java
@@ -1,20 +1,26 @@
 package com.example.notification_service.service;
 
 
+import com.example.expense_tracker.common.UserCreatedEvent;
 import com.example.notification_service.dto.requestDto.NotificationRequestDTO;
 import com.example.notification_service.dto.requestDto.NotificationUpdateReadStatusDTO;
 import com.example.notification_service.dto.responseDto.NotificationResponseDTO;
+import com.example.notification_service.enums.NotificationType;
 import com.example.notification_service.exception.ResourceNotFound;
 import com.example.notification_service.mapper.NotificationMapper;
 import com.example.notification_service.model.Notification;
 import com.example.notification_service.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -54,5 +60,25 @@ public class NotificationServiceImpl implements NotificationService {
         Notification notification = notificationRepository.findByNotificationId(notificationId)
                 .orElseThrow(() -> new ResourceNotFound("Notification not found with ID: " + notificationId));
         notificationRepository.delete(notification);
+    }
+
+
+
+
+    @KafkaListener(
+            topics = "${kafka.topic.name}",
+            groupId = "${kafka.topic.group-id}",
+            containerFactory = "userCreatedEventKafkaListenerContainerFactory"
+    )
+    public void consumeUserCreatedEvent(UserCreatedEvent event) {
+        Notification notification = new Notification();
+        notification.setNotificationId(UUID.randomUUID().toString());
+        notification.setUserId(event.getUserId());
+        notification.setMessage("Welcome to Personal Finance App! Start tracking your finances today.");
+        notification.setType(NotificationType.SYSTEM);
+        notification.setRead(false);
+
+        log.info("-----------> Consumed UserCreatedEvent: {}", event);
+        notificationRepository.save(notification);
     }
 }
